989788dc4c620c41b5334c4790a6f96c2698f5c8
zdcthomas <zdcthomas@yahoo.com>
2018-05-15 14:06:51 -0600

    WIP implements students views

--- spec/features/students/edit_spec.rb
+++ spec/features/students/edit_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 describe 'A User' do
   describe 'visits students/:id/edit' do
     it 'should have a name field' do
-      student = Student.new(name: "Johnny")
+      student = Student.create!(name: "Johnny")
       visit edit_student_path student
 
       expect(page).to have_field('student[name]')
     end
     it 'should redirect to corresponding show page after submission' do
-      student = Student.new(name: "Johnny")
+      student = Student.create!(name: "Johnny")
       visit edit_student_path student
 
       name = 'Joe DiMaggio'
@@ -19,7 +19,7 @@ describe 'A User' do
     end
     it 'should show the student name after submission' do
 
-      student = Student.new(name: "Johnny")
+      student = Student.create!(name: "Johnny")
       visit edit_student_path student
 
       name = 'James Baldwin'
