8f68d679795616b475b343b6705c39e8ac6201f2
zdcthomas <zdcthomas@yahoo.com>
2018-05-15 13:47:52 -0600

    TESTS student index show page links

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -14,5 +14,18 @@ describe 'A User' do
       expect(page).to have_content(student3.name)
       expect(page).to have_content(student4.name)
     end
+    context 'the name of each student' do
+      it 'should be a link to that students show page' do
+      student1 = Student.create!(name: 'Greg')
+      student2 = Student.create!(name: 'Martha')
+      student3 = Student.create!(name: 'Gertrude')
+      student4 = Student.create!(name: 'Marshall')
+
+      visit students_path
+      click_on "#{student1.name}"
+
+      expect(current_path).to eq(student_path student1)
+      end
+    end
   end
 end
